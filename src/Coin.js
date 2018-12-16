eb33b5348ab94142b5bb1f617bb10b8f477e0d95
Mati <matibat@gmail.com>
2018-12-15 23:42:43 -0300

    Working coin flipper

--- src/Coin.js
+++ src/Coin.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import './Coin.css';
-import coinImage from './Coin.png';
+import headCoin from './head-coin.png';
+import tailsCoin from './tails-coin.png';
+import spinningCoin from './spinning-coin.png';
 
 class Coin extends React.Component { 
     FACES = ['HEAD', 'TAILS'];
-    state = { facing: this.getRandomFace() }
-
-    flip = () => {
-        this.setState({ facing: this.getRandomFace() });
+    SPINNING = 'SPINNING';
+    ROTATION_TIME = 700;
+    IMAGES = {
+        'HEAD': headCoin,
+        'TAILS': tailsCoin,
+        'SPINNING': spinningCoin,
     }
 
-    getRandomFace = () => {
+    state = { facing: this.FACES[0] };
+
+    flip = () => {
+        this.setState({ facing: this.SPINNING });
+        setTimeout(
+            this.setRandomFace,
+            this.ROTATION_TIME
+        );
+    }
+
+    setRandomFace = () => {
         let randomIndex = Math.floor(Math.random() * this.FACES.length);
-        return this.FACES[randomIndex];
+        this.setState({ facing: this.FACES[randomIndex] });
+    }
+
+    isSpinning = () => {
+        return this.state.facing === this.SPINNING;
     }
 
     render() {
         return (
-            <div className="coin-holder">
-                <img src={coinImage} alt="coin" className="coin" />
+            <div className="coin-holder" onClick={ this.flip }>
+                <img 
+                    src={ this.IMAGES[this.state.facing] }
+                    alt="coin"
+                    className={ "coin" + (this.isSpinning() ? '-spin' : '') }
+                />
             </div>
         );
     }
